91f962f2025046c3fca08caf12e8208aca744a18
KlausMu <kmusch@gmail.com>
2024-02-25 19:06:29 +0100

    log IP address when connected to WiFi

--- Platformio/src/hardware/mqtt.cpp
+++ Platformio/src/hardware/mqtt.cpp
@@ -22,6 +22,7 @@ void WiFiEvent(WiFiEvent_t event){
   // Set status bar icon based on WiFi status
   if (event == ARDUINO_EVENT_WIFI_STA_GOT_IP || event == ARDUINO_EVENT_WIFI_STA_GOT_IP6) {
     if (WifiLabel != NULL) {lv_label_set_text(WifiLabel, LV_SYMBOL_WIFI);}
+    Serial.printf("WiFi connected, IP address: %s\r\n", WiFi.localIP().toString().c_str());
 
   } else if (event == ARDUINO_EVENT_WIFI_STA_DISCONNECTED) {
     if (WifiLabel != NULL) {lv_label_set_text(WifiLabel, "");}
